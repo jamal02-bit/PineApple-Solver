f230d041aa19e6bec5196cf1cc0541ef122268f8
ccollins80 <cameronbcollins26@gmail.com>
2025-02-04 16:15:23 -0500

    changed selection print statements

--- main.py
+++ main.py
@@ -37,15 +37,14 @@ def printOptions():
     printOptions() allows the user to select whether to randomly generate 15 cards
                    or to input 15 of their own in a specific format.
     """
-    isRandom = input("Please enter 'random' if you want the 15 cards randomly generated.\n")
+    isRandom = input("Please enter 'r' if you want the 15 cards randomly generated.\nOtherwise, enter 15 cards in the from `(R)ank(s)uit` and don't add a space inbetween the commas.\n")
 
-    if (isRandom == "random"):
+    if (isRandom == "r"):
         print("Random generation selected.")
         # send a call to randomGeneration()
         # and print generated cards DeckGenerator::generateFantasyLandDeck()
     else:
-        cards = input("Please enter 15 cards in the from `(R)ank(s)uit` and don't add a space inbetween the commas.\n")
-        print(cards)
+        print("User generation selected.")
         # Send cards to a parser
 
 # Call to functions
